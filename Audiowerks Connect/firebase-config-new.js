3ebe1f1f467558913aaee9b9af78deff78168141
Erin Castro <sendgcastro@gmail.com>
2025-07-10 04:03:06 +0800

    chat not done

--- Audiowerks Connect/firebase-config-new.js
+++ Audiowerks Connect/firebase-config-new.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD8qE012gHRnE60RrehkasVnMDYkxjrz_s",
+    apiKey: "AIzaSyBIJf4WsNG3_1C-9WjmMhuI-X8kLZdK958",
     authDomain: "audiowerks-8e6bf.firebaseapp.com",
     databaseURL: "https://audiowerks-8e6bf-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "audiowerks-8e6bf",
@@ -7,6 +7,6 @@ const firebaseConfig = {
     messagingSenderId: "462605181203",
     appId: "1:462605181203:web:b93b118cc9b63c77907ae6",
     measurementId: "G-93R061TZ32"
-};
+  };
 
 firebase.initializeApp(firebaseConfig); 
